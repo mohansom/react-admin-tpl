a04d22e3cbc3260ffab4bf6dab0652860f8b13ab
FFFXUE <18897960357@163.com>
2019-09-19 00:20:18 +0800

    拆分 Reducer

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -2,7 +2,7 @@
  * @Author: liujianfeng
  * @Date: 2019-09-18 19:36:54
  * @LastEditors: liujianfeng
- * @LastEditTime: 2019-09-18 19:55:50
+ * @LastEditTime: 2019-09-19 00:17:55
  */
 import * as user from './action-type';
 
@@ -15,14 +15,27 @@ const defaultState = {
     isMobileDev:false
 }
 
-
-export const getUserInfo = (state = defaultState,action = {}) => {
+function userInfo(state,action){
     switch(action.type){
         case user.USER_INFO:
-            return {...state,userInfo:{...state.userInfo,...action}}
-        case user.USER_DEV:
-            return Object.assign({},state,{isMobileDev:action.isMobileDev})
+            return {...state,...action}
         default:
-            return {...state}
+            return state
+    }
+}
+
+function isMobileDev (state,acition){
+    switch(acition.type){
+        case user.USER_DEV:
+            return acition.isMobileDev
+        default:
+            return state
+    }
+}
+
+export const getUserInfo = (state = defaultState,action = {}) => {
+    return{
+        userInfo:userInfo(state.userInfo,action),
+        isMobileDev:isMobileDev(state.isMobileDev,action)
     }
 }
