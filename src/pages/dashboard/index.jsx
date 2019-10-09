4f8186fc7205864cfa1de8d3a95b10cd7a759114
FFFXUE <18897960357@163.com>
2019-10-09 22:58:31 +0800

    prop-types

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+const Greeting  = (props) => {
+    return(
+        <div>
+            {props.name}
+        </div>
+    )
+}
+
+Greeting.propTypes = {
+    name:PropTypes.string
+}
+
 class Home extends React.Component{
     render(){
         return(
-            <div>
-                home
-            </div>
+            <Greeting name={123} />
         )
     }
 }
